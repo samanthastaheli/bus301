e3a679b8ce5268d3cc3046215d8d4a2a09f5e2cb
samanthastaheli <sta180011@byui.edu>
2021-06-07 23:30:33 -0600

    presentable site

---
+++ script-main.js
@@ -0,0 +1,11 @@
+// chemsitry similator funcitions
+/*
+add partcles (circles)
+have circles move 
+heat/cold lever
+    like truck level
+    filp up = heat
+    middle = none
+    flip down = cold
+circles speed can be adjusted 
+*/
